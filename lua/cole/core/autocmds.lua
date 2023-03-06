b28c99c6d522e65c386447e4412f02d6bad10b09
Cole <czimm203@gmail.com>
2023-03-06 15:25:10 -0600

    thing

--- lua/cole/core/autocmds.lua
+++ lua/cole/core/autocmds.lua
@@ -2,5 +2,5 @@ local api = vim.api
 
 api.nvim_create_autocmd(
     "FileType",
-    {pattern = {"typescript","javascript","html"}, command = "setlocal et ts=2 sw=2 sts=2"}
+    {pattern = {"typescript","javascript","html","svelte"}, command = "setlocal et ts=2 sw=2 sts=2"}
 )
